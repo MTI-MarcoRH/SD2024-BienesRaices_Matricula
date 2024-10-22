98d34e2b8a0d963166279613d5aac795a5ab8e49
Marco A. Ramírez Hdez. <marco.ramirez@utxicotepec.edu.mx>
2024-10-22 13:40:52 -0600

    Practica 09
    Actualización de estandar de tipado para código de NodeJS

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require('express');  //Importar la libreria para crear un serviror web
+// ECMA Script 6
+// CommonJS
+import express from 'express';
 
+//const express = require('express');  //Importar la libreria para crear un serviror web - CommonJS
 // Instanciar nuestra aplicación web
 const app = express()
 
@@ -9,6 +12,7 @@ app.listen(port, ()=>{
     console.log(`La aplicación ha iniciado en el puerto: ${port}` );
 })
 
+//Routing - Ennrutamiento.
 // Probamos las rutas para poder presentar mensajes al usuario a través del navegador
 app.get("/", function(req, res){
     res.send("Hola Mundo desde Node, a través del Navegador");
