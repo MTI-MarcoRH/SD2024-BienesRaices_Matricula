e671d98479d204a7789760c1b8c9e50b481c2f34
Marco A. Ramírez Hdez. <marco.ramirez@utxicotepec.edu.mx>
2024-10-24 17:44:14 -0600

    Practica 11
    Instalación y configuración de PUG y Tailwind

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const formularioLogin = (request, response) =>  {
+    response.render('auth/login', {
+        
+    })};
+
+const formularioRegister = (request, response) =>  {
+        response.render('auth/register', {
+            
+        })};
+
+const formularioPasswordRecovery = (request, response) =>  {
+    response.render('auth/passwordRecovery', {
+                
+     })};
+     
+export {formularioLogin, formularioRegister, formularioPasswordRecovery}
--- index.js
+++ index.js
@@ -10,6 +10,13 @@ import userRoutes from './routes/userRoutes.js'
 
 const app = express()
 
+// Configurar Templeate Engine - PUG
+app.set('view engine', 'pug')
+app.set('views','./views')
+
+//Definir la carpeta ública de recursos estáticos (assets)
+app.use(express.static('./public'));
+
 // Configuramos nuestro servidor web
 const port =3000;
 app.listen(port, ()=>{
